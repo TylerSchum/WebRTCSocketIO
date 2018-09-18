0a811563769837f0cbf9a348d178ef1e98116764
tschumdev <tschumdev@gmail.com>
2018-09-18 15:15:58 -0400

    skip users in iteration

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ io.on('connection', function (socket) {
             }
             config.peer_id = socket.id;
             for (id in channels[channel]) {
-                if (id == 'users') {
+                if (id !== 'users') {
                     channels[channel][id].emit('addPeer-room', {'peer_id': socket.id, 'should_create_offer': false});
                     socket.emit('addPeer-room', {'peer_id': id, 'should_create_offer': true});
                     console.log("what  is this  id -> ", id);
